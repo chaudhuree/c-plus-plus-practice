d4a49e306937a21aaa2fcb710217c65d28956636
chaudhuree <chaudhuree00@gmail.com>
2022-12-13 02:32:31 +0600

    docs: c++ basic data types int and double and exercise one

---
+++ basics/01.variables.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+int main(){
+  int file_size=100;
+  double file_size_double=99.99;
+
+  // cout << file_size;
+
+
+  // cout << file_size_double;
+  // swaping exercise
+  int a=1;
+  int b=2;
+
+  int temp;
+  temp=a;
+  a=b;
+  b=temp;
+  cout<<a;
+  // cout<<b;
+}
