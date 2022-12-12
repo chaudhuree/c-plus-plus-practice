e49b5b0eadd369b12071c9bf2e46070e5fc1ceea
chaudhuree <chaudhuree00@gmail.com>
2022-12-13 03:02:34 +0600

    docs : some mathematical operations

---
+++ 02.mathematical.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+ using namespace std;
+
+ int main(){
+  // constant declaration 
+  const double PI=3.14;
+
+  // to return float for division one of the variable should be double
+  double a=10;
+  int b=3;
+  double result=1/3;
+
+  // pre increment and post increment
+  int z= a++; //here z=10; a=11 -->post increment
+  int y= ++a; //here y=12; a=12 --->pre increment
+
+ }
