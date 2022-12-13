70bf8d582f15711e616d82bc7fe10efd5178ef07
chaudhuree <chaudhuree00@gmail.com>
2022-12-14 04:53:19 +0600

    exercise print

--- clion/main.cpp
+++ clion/main.cpp
@@ -3,10 +3,14 @@
 using namespace std;
 
 int main() {
-    int x = 10;
-    int y = 5;
+    int sales = 95000;
+    double salesTax = 0.04;
+    double countryTax = 0.02;
 
-    cout << "x=" << x << endl
-         << "y= " << y;
+    cout << "sales=" << sales << endl
+         << "sales tax= " << salesTax << endl
+         << "country tax= " << countryTax << endl
+         << "net tax= " << sales * (salesTax + countryTax) << endl
+         << "net balance= " << sales - sales * (salesTax + countryTax) << endl;
     return 0;
 }
