9671ad164b35c14a4406c33d4138f331636dc6de
chaudhuree <chaudhuree00@gmail.com>
2022-12-14 04:30:54 +0600

    exercise

---
+++ clion/exercise.cpp
@@ -0,0 +1,14 @@
+//
+// Created by User on 12/14/2022.
+//
+#include <iostream>
+
+int main(){
+    double x=10;
+    double y=5;
+
+    double z=(x+10)/(3*y);
+    std::cout<<z;
+
+    return 0;
+}
--- clion/main.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-
-int main() {
-    double a = 10;
-    int b = 3;
-    double result = a / b;
-    std::cout << result;
-    return 0;
-}
