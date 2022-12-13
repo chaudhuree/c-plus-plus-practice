b2ed06d7cb4905469098615c9db9b0301f43111e
chaudhuree <chaudhuree00@gmail.com>
2022-12-14 05:02:08 +0600

    input to the console code

--- clion/main.cpp
+++ clion/main.cpp
@@ -3,14 +3,9 @@
 using namespace std;
 
 int main() {
-    int sales = 95000;
-    double salesTax = 0.04;
-    double countryTax = 0.02;
-
-    cout << "sales=" << sales << endl
-         << "sales tax= " << salesTax << endl
-         << "country tax= " << countryTax << endl
-         << "net tax= " << sales * (salesTax + countryTax) << endl
-         << "net balance= " << sales - sales * (salesTax + countryTax) << endl;
+    int x, y;
+    cout << "enter the value for x and y: " << endl;
+    cin >> x >> y;
+    cout << x + y;
     return 0;
 }
