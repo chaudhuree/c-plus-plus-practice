1ab3e9b2b8469486bb1e629f7f46a2dd1f1f9573
chaudhuree <chaudhuree00@gmail.com>
2022-12-15 17:40:52 +0600

    docs: number  types

---
+++ basics/04.datatypes.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+  short number0 = 12312;
+  int number1 = 13434;
+  double number2 = 2.35;
+  float number3 = 3.545F;
+  long number4 = 43434L;
+  bool number5 = true;
+  // note: it also has type auto we can replace int/float etc with auto
+  return 0;
+}
