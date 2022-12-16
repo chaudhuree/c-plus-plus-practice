55e891759d35fe6b65341fed79b91171c1905905
chaudhuree <chaudhuree00@gmail.com>
2022-12-16 23:44:50 +0600

    docs: conditional operator if else if
    ternary
    
    switch

---
+++ basics/12.condition(if,ternary,switch).cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+int main()
+{
+  /*
+  docs:(if, else if, else)
+  note: syntax
+  if (condition1) {
+    // block of code to be executed if condition1 is true
+  } else if (condition2) {
+    // block of code to be executed if the condition1 is false and condition2 is true
+  } else {
+    // block of code to be executed if the condition1 is false and condition2 is false
+  }
+
+  docs: ternary operator
+  note:(syntax)
+  variable = (condition) ? expressionTrue : expressionFalse;
+
+  docs: switch (condition)
+  note:(syntax)
+  switch(expression) {
+  case x:
+    // code block
+    break;
+  case y:
+    // code block
+    break;
+  default:
+    // code block
+}
+  */
+
+  return 0;
+}
