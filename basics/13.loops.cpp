c95f27462fade4de5b399985cba55db00466ad4c
chaudhuree <chaudhuree00@gmail.com>
2022-12-18 01:45:34 +0600

    finish: loops and break and continue

--- basics/13.loops.cpp
+++ basics/13.loops.cpp
@@ -20,11 +20,48 @@ int main()
   //   cout << i << "\n";
   // }
 
-  // for in range
-  int number[] = {23, 45, 64};
-  for (int i : number)
-  {
-    cout << i << endl;
-  }
+  // docs: for in range
+  // int number[] = {23, 45, 64};
+  // for (int i : number)
+  // {
+  //   cout << i << endl;
+  // }
+
+  // docs: while
+  // int i = 0;
+  // while (i < 5)
+  // {
+  //   cout << i << "\n";
+  //   i++;
+  // }
+
+  // docs: do while
+  // int i = 0;
+  // do
+  // {
+  //   cout << i << "\n";
+  //   i++;
+  // } while (i < 5);
+
+  // docs: break/continue
+
+  // note: break
+  //   for (int i = 0; i < 10; i++) {
+  //   if (i == 4) {
+  //     break;
+  //   }
+  //   cout << i << "\n";
+  // }
+
+  // note: continue
+  // for (int i = 0; i < 10; i++)
+  // {
+  //   if (i == 4)
+  //   {
+  //     continue;
+  //   }
+  //   cout << i << "\n";
+  // }
+
   return 0;
 }
