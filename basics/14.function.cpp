846e05e4b3440b43e331d8313f1f684721149dba
chaudhuree <chaudhuree00@gmail.com>
2022-12-20 20:06:16 +0600

    docs: simple function call

---
+++ basics/14.function.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+void greet()
+{
+  cout << "good evening" << endl;
+}
+int main()
+{
+  greet();
+
+  return 0;
+}
