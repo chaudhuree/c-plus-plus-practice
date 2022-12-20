08db792126998f195e265ebec96b1863e74ce676
chaudhuree <chaudhuree00@gmail.com>
2022-12-20 20:15:12 +0600

    docs: function with parameters and pre define

--- basics/14.function.cpp
+++ basics/14.function.cpp
@@ -4,14 +4,31 @@
 #include <ctime>
 
 using namespace std;
-
+// function define without parameter
 void greet()
 {
   cout << "good evening" << endl;
 }
+// function parameters
+int addNum(int a, int b)
+{
+  return a + b;
+}
+// multiple num
+int mulNum(int, int);
 int main()
 {
   greet();
 
+  int resultAdd = addNum(1, 1);
+  cout << resultAdd << endl;
+  int resultmult = mulNum(2, 3);
+  cout << resultmult << endl;
+
   return 0;
+}
+int mulNum(int a, int b)
+{
+  int multiplication = a * b;
+  return multiplication;
 }
